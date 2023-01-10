de868cf36c6ad92a599c7e6122f072743f1e6009
xartv <xvasartx@gmail.com>
2023-01-10 18:03:04 +0400

    push commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+		<label htmlFor="checkbox">
+			<input id='checkbox' type="checkbox"/>
+			<div className="check"></div>
+		</label>
+  );
+}
+
+export default App;
